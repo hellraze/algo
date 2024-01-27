813495797de5c0e45212211ac99ea2e50365726b
Aidar Yarullin <121501247+hellraze@users.noreply.github.com>
2024-01-27 20:46:49 +0300

    Update lab6.go

--- lab/lab6.go
+++ lab/lab6.go
@@ -6,16 +6,16 @@ import (
 )
 
 func findPresidentMentions(hymn, name string) (int, []int) {
-	// Инициализация списка для хранения индексов упоминаний имени президента
+	
 	var nameMention []int
 
-	// Проход по тексту гимна для поиска упоминаний имени президента
+	
 	index := 0
 	for {
-		// Поиск индекса первого вхождения имени президента в оставшейся части гимна
+		
 		mentionIndex := strings.Index(hymn[index:], name)
 
-		// Если вхождение найдено, добавляем индекс в список и обновляем индекс для следующего поиска
+		
 		if mentionIndex != -1 {
 			nameMention = append(nameMention, index+mentionIndex)
 			index = index + mentionIndex + 1
@@ -25,17 +25,17 @@ func findPresidentMentions(hymn, name string) (int, []int) {
 		}
 	}
 
-	// Возвращаем количество упоминаний и список индексов
+	
 	return len(nameMention), nameMention
 }
 
 func main() {
-	// Ввод данных
+	
 	var hymn, presidentName string
 	fmt.Scan(&hymn)
 	fmt.Scan(&presidentName)
 
-	// Получение результатов и вывод
+	
 	count, indices := findPresidentMentions(hymn, presidentName)
 	fmt.Println(count)
 	for i := 0; i < len(indices); i++ {
